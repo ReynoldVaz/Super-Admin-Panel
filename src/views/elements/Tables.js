6ac0afa9f8555ade05850239480b930f3e6de488
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-28 14:47:31 +0530

    Update Tables.js

--- src/views/elements/Tables.js
+++ src/views/elements/Tables.js
@@ -1,63 +1,33 @@
-import React, {Component} from 'react';
-import {
-  Card,
-  CardBody,
-  Table
-} from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardBody, Table, Button } from 'reactstrap';
 
 class TablePage extends Component {
   render() {
     return (
-        <Card>
-            <CardBody>
-                Basic Table
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                Striped Table
-                <Table striped>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
+      <Card>
+        <CardBody>
+          Manage Centres
+          <Table>
+            <thead>
+              <tr>
+                <th>Sr No.</th>
+                <th>Centre Name</th>
+                <th>Password</th>
+                <th>Email</th>
+                <th>Activity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                  <Button color="primary">Active</Button>
+                </td>
+              </tr>
+              {/*
                         <tr>
                             <td>2</td>
                             <td>Jacob</td>
@@ -69,43 +39,74 @@ class TablePage extends Component {
                             <td>Larry</td>
                             <td>the Bird</td>
                             <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                Hover Table
-                <Table hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    )
+                        </tr>*/}
+            </tbody>
+          </Table>
+          Striped Table
+          <Table striped>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+          Hover Table
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    );
   }
 }
 
