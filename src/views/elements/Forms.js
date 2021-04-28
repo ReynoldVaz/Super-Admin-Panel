ee92df9d01193a3846a4946dcb504a6d2c7f61d0
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-28 14:46:39 +0530

    Update Forms.js

--- src/views/elements/Forms.js
+++ src/views/elements/Forms.js
@@ -1,44 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText, Col, Row, Card, CardBody } from 'reactstrap';
 
 export default class FormsPage extends Component {
   constructor() {
     super();
-    this.state = { }
+    this.state = {};
   }
 
   render() {
     return (
       <Row>
-          <Col md={{ size: 8, offset: 2 }}>
-            <Card>
-                <CardBody>
-          <Form>
-        <FormGroup>
-          <Label for="name">Name</Label>
-          <Input type="text" name="name" id="name" placeholder="First and Last" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Email</Label>
-          <Input type="text" name="email" id="email"placeholder="Enter Email">
-            
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelectMulti">Password</Label>
-          <Input type="text" name="password" id="password"placeholder="Enter password">
-            
-          </Input>
-        </FormGroup>
-        
-        
-        
-        <Button>SAVE</Button>
-      </Form>
-      </CardBody>
-      </Card>
-      </Col>
+        <Col md={{ size: 8, offset: 2 }}>
+          <Card>
+            <CardBody>
+              <Form>
+                <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input type="text" name="name" id="name" placeholder="Enter car centre name" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleSelect">Email</Label>
+                  <Input type="text" name="email" id="email" placeholder="Enter Email"></Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleSelectMulti">Password</Label>
+                  <Input type="text" name="password" id="password" placeholder="Enter password"></Input>
+                </FormGroup>
+
+                <Button>Register Car Wash Admin</Button>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
       </Row>
-    )
+    );
   }
 }
